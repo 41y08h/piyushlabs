9b7b51d069c3665337e11bfa35a0a1e08d57f752
Piyush <piyush41y08h@gmail.com>
2021-03-25 15:02:39 +0530

    Add titles

--- layouts/index.js
+++ layouts/index.js
@@ -1,8 +1,14 @@
 import Layout from "components/Layout";
+import Seo from "../components/Seo";
+import config from "../site.config";
 
 export default function Index({ frontMatter, children }) {
   return (
     <Layout>
+      <Seo
+        title={`${frontMatter.title} | ${config.title}`}
+        description={frontMatter.description}
+      />
       <div className="mt-12">
         <time className="text-sm text-gray-400">
           {new Date(frontMatter.date).toDateString()}
